95aa361926d82c600ee22dbef224e27f82ae3bc2
Hid3kii <riqueoka@gmail.com>
2026-08-05 15:47:39 -0300

    exibição de estoque

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 #Importa a classe de produtos
 from models.produtos import Produto
 
-produto1 = Produto(1, "tomate", 150.00, 10)
+tomate = Produto(1, "tomate", 150.00, 10)
 
 print("Produto 1")
-print("Id: ", produto1.id)
-print("Nome: ", produto1.nome)
-print("Preço: R$", produto1.preco)
-print("Estoque: ", produto1.estoque)
+print("Id: ", tomate.id)
+print("Nome: ", tomate.nome)
+print("Preço: R$", tomate.preco)
+print("Estoque: ", tomate.estoque)
+
+print("--- Estado Inicial ---")
+tomate.exibir_detalhes()
+
+print("\n--- Testando Recebimento de Estoque ---")
+tomate.adicionar_estoque(5)
+tomate.exibir_detalhes() # Esperado: 15 unidades
+
+print("\n--- Testando Venda Com Sucesso ---")
+tomate.remover_estoque(3)
+tomate.exibir_detalhes() # Esperado: 12 unidades
+
+print("\n--- Testando Venda Sem Estoque Suficiente ---")
+tomate.remover_estoque(20) # Deve disparar o aviso de estoque insuficiente!
+tomate.exibir_detalhes() # Deve continuar com 12 unidad
--- models/produtos.py
+++ models/produtos.py
@@ -5,4 +5,21 @@ class Produto:
         self.preco = preco
         self.estoque = estoque
 
+    def exibir_detalhes(self):
+        print(f"ID: {self.id} | Nome: {self.nome} | Preço: R${self.preco:.2f} | Estoque: {self.estoque}")
 
+    def adicionar_estoque(self, quantidade):
+        if quantidade > 0:
+            self.estoque += quantidade
+            print(f"Adicionadas {quantidade} unidades ao estoque de {self.nome}.")
+        else:
+            print("A quantidade para adicionar deve ser maior que zero!")
+
+    def remover_estoque(self, quantidade):
+        if quantidade <= 0:
+            print("A quantidade para remover deve ser maior que zero!")
+        elif quantidade <= self.estoque:
+            self.estoque -= quantidade
+            print(f"Removidas {quantidade} unidades do estoque de {self.nome}.")
+        else:
+            print(f"Venda não permitida! Estoque insuficiente para {self.nome}. Disponível: {self.estoque}")
